18c7fc3151f1332b451e9a07a0a1a2c5218edf93
fhrony <fhrony43@gmail.com>
2020-08-06 09:40:04 -0800

    variable complate

---
+++ variable.js
@@ -0,0 +1,17 @@
+//use  of var let const
+
+var player = 'farhad'; 
+//var used early in javascript. in modern javascript for varible diclaration use const or let.
+
+
+let age = 25;
+
+const car = {color : "red", weight : 50} 
+
+const name = 'rony';
+
+console.log(car.color);
+
+for (let i = 0; i < 10 ; i++){
+    console.log(i);
+}
