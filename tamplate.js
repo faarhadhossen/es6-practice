173cceab578d9ce34ed4ee1e03ecef80190d2156
fhrony <fhrony43@gmail.com>
2020-08-06 10:05:10 -0800

    tamplate complate

---
+++ tamplate.js
@@ -0,0 +1,25 @@
+// const fName = 'Justin';
+// const lName = 'Baiber';
+
+// const fullName = fName + ' ' + lName + 'is a famous singer.';
+
+// console.log(fullName);
+
+
+const fName = 'Justin';
+const lName = 'Baiber';
+
+const fullName = `${fName} ${lName} is a famous singer.`;
+
+console.log(fullName);
+
+
+
+
+//multiline string
+
+let letter = `Hello Bruce,
+How are you.
+I have received your letter yesterday.`
+console.log(letter);
+
