05a0e604ae9169bbc75518b3f5f08123c01343fd
fhrony <fhrony43@gmail.com>
2020-08-06 10:23:30 -0800

    tamplate and arrow function complate

---
+++ arrow.js
@@ -0,0 +1,20 @@
+// regular function 
+
+// function sum(num1 , num2){
+//     return num1 + num2;
+// } //function expression
+
+
+// let sum = function (num1, num2){
+//     return num1 + num2;
+// } //function declaration . this type of function dont need any name. function use as a variable.
+
+// let sum = (num1,num2)=>{
+//     return num1 + num2;
+// } //arrow function.
+
+
+let duble = num => num * 2;
+
+const result = duble(7);
+console.log(result);
